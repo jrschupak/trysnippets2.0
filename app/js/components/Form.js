cb2b2b0055786b636165c283557ea0eac7183376
Jonathan Schupak <jrschupak@gmail.com>
2016-09-06 12:20:17 -0400

    deleted form

--- app/js/components/Form.js
+++ app/js/components/Form.js
@@ -55,11 +55,7 @@ class Form extends React.Component {
           <div className="wrapper-form col-md-6 col-md-offset-3 text-center">
             <div className="box" style={box} onClick={this.props.boxIsClicked} value={0}>Box for snippet 1</div>
             <div className="box" style={box} onClick={this.props.boxIsClicked} value={1}>Box for snippet 2</div>
-            <form className="checkboxes" style={form}>    
-              <input  type="checkbox" value={0} style={input}/><span style={inputSpan}>Snippet1</span>           
-              <input  type="checkbox" value={1} style={input}/><span style={inputSpan}>Snippet2</span>
-            </form>
-            <button onClick={this.props.isChecked} style={submitBtn}>Submit</button>
+            
           </div>
           
         </div>
