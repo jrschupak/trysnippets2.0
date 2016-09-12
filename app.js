9d5859c6becb4e149f2f2db96becdf145375009d
Jonathan Schupak <jrschupak@gmail.com>
2016-09-12 14:34:43 -0400

    app.js with express server

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.static('./dist'));
+app.listen(PORT, function(err) {
+  if (err) {
+    console.log('error starting server');
+    console.log(err);
+  } else {
+    console.log('server up and running on port', PORT);
+  }
+});
