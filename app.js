793a9bfe5befef3b445d8088f38361189a27c713
Jonathan Schupak <jrschupak@gmail.com>
2016-09-13 13:40:40 -0400

    added dist folder

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.use(express.static('/app'));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(_dirname, 'index.html'))
-});
+app.use(express.static('./dist'));
 app.listen(PORT, function(err) {
   if (err) {
     console.log('error starting server');
