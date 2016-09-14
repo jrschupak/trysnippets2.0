7692996a36992925f0ee78bf9d01922bca453624
Jonathan Schupak <jrschupak@gmail.com>
2016-09-14 15:59:56 -0400

    change snippet state to array to sep lines and put each line in the snippet <p>

--- app/js/components/Snippets.js
+++ app/js/components/Snippets.js
@@ -7,22 +7,23 @@ class Snippets extends React.Component {
   constructor () {
     super();
     this.state = {
-      snippet1: "<div>This is snippet 1</div>",
+      snippet1: ["<div>This is snippet 1</div>",  "<div>Part2 of snippet</div>"],
       snippet2: "<div>This is snippet 2</div>"
      }
     } 
   
   componentWillMount(){
     console.log("Snippets.js ", this.props.snippets);
-
   }
 
+  componentDidMount(){
+    console.log("snippet1", this.state.snippet1[1]);
+  }
 
 
   snippetsContainer(){
     var snipCont = document.querySelector('snippets-container');
     console.log(snipCont);
-    
   }
 
   
@@ -37,7 +38,7 @@ class Snippets extends React.Component {
     return (
       <div>
         <div className="snippets-cont">
-          <p id={0} style={displayNone}>{this.state.snippet1}</p>
+          <p id={0} style={displayNone}>{this.state.snippet1[0]} <br/> {this.state.snippet1[1]}</p>
           <p id={1} style={displayNone}>{this.state.snippet2}</p>
         </div>
 
