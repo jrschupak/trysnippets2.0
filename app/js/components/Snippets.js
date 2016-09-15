c5bb88b8bc6113c97f611112978c5b30828164ae
Jonathan Schupak <jrschupak@gmail.com>
2016-09-15 10:50:55 -0400

    map function works

--- app/js/components/Snippets.js
+++ app/js/components/Snippets.js
@@ -7,7 +7,7 @@ class Snippets extends React.Component {
   constructor () {
     super();
     this.state = {
-      snippet1: [{id: 0, content: "<div>This is snippet 1</div>"}, {id: 1, content:  "<div> part 2 of snippet 1 </div>"}],
+      snippet1: { snippetArr: [{id: 0, content: "<div>This is snippet 1</div>"}, {id: 1, content:  "<div> part 2 of snippet 1 </div>"}]},
       snippet2: "<div>This is snippet 2</div>"
      }
     } 
@@ -31,21 +31,22 @@ class Snippets extends React.Component {
 
     var displayNone = {
     display: "none"
-    } 
+    }
+
     var display = {
 
     }
     return (
       <div>
         <div className="snippets-cont">
-          <p id={0} style={displayNone}>{this.state.snippet1[0]} <br/> {this.state.snippet1[1]}</p>
+          <p id={10} style={displayNone}>{this.state.snippet1[0]} <br/> {this.state.snippet1[1]}</p>
           <p id={1} style={displayNone}>{this.state.snippet2}</p>
         </div>
 
-        <div className="snippets-container" style={snippet}>{this.state.snippet1.map(function(line) {
+        <div className="snippets-container" id={0} style={displayNone}>{this.state.snippet1.snippetArr.map(function(line) {
               console.log(line);
               return <div  key={line.id} className='zip-comp'>
-                        <p>{line.snippet}</p>
+                        <p>{line.content}</p>
                      </div>
               })}
         </div>
