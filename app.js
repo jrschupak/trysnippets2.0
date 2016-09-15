a239daf5e66f549eddfa4f66c2273ff3c90b1e5d
Jonathan Schupak <jrschupak@gmail.com>
2016-09-15 12:24:24 -0400

    npm install and node.js update new bundle.js file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.use(express.static('./app'));
+app.use(express.static('./dist'));
 app.listen(PORT, function(err) {
   if (err) {
     console.log('error starting server');
