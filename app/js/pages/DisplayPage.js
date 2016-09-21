dacbeefad7bfd68f10040e54ca9b2fd81fd12e73
Jonathan Schupak <jrschupak@gmail.com>
2016-09-21 15:07:55 -0400

    setup displaypage

--- app/js/pages/DisplayPage.js
+++ app/js/pages/DisplayPage.js
@@ -1,1 +1,38 @@
-DisplayPage.js
+
+import React         from 'react';
+import {Link}        from 'react-router';
+import DocumentTitle from 'react-document-title';
+
+const propTypes = {
+  currentUser: React.PropTypes.object
+};
+
+class DisplayPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <DocumentTitle title="Snippets">
+        <section className="display-page">
+
+          <div>
+            
+          </div>
+
+          <div>
+            
+          </div>
+
+        </section>
+      </DocumentTitle>
+    );
+  }
+
+}
+
+DisplayPage.propTypes = propTypes;
+
+export default DisplayPage;
