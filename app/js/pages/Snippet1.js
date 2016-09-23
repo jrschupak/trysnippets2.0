f13b531a0d35b9432c6f0b7043a12d8366bafe50
Jonathan Schupak <jrschupak@gmail.com>
2016-09-23 14:05:20 -0400

    position of home btn and snip-container styling

--- app/js/pages/Snippet1.js
+++ app/js/pages/Snippet1.js
@@ -12,7 +12,7 @@ class Snippet1 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      snippet1: { snippetArr: [{id: 0, content: "contstructor () {"}, {id: 1, content:  "super();"}, {id: 2, content: "this.state = {"}, {id: 3, content: "}"}, {id: 4, content: "}"} ]},
+      snippet1: { snippetArr: [{id: 0, content: "contstructor ( ) {"}, {id: 1, content:  "super();"}, {id: 2, content: "  this.state = {"}, {id: 3, content: "  }"}, {id: 4, content: " }"} ]},
       snippet2: { snippetArr: [{id:0, content: "<div>"}, {id: 2, content: "This is snippet 2"}, {id: 3, content: "</div>"}]}
      }
   }
@@ -20,7 +20,6 @@ class Snippet1 extends React.Component {
   render() {
 
   	var lines = {
-      color: "black",
       width: "90%",
       textAlign: "left",
       paddingLeft: "20px"
@@ -29,7 +28,18 @@ class Snippet1 extends React.Component {
     var homeBtn = {
     	height: "50px",
     	width: "100px",
-    	float: "right"
+    	position: "absolute",
+    	right: "10px",
+    	top: "50px"
+    }
+
+    var snipCont = {
+    	background: "black",
+    	color: "yellow",
+    	position: "relative",
+    	top: "150px",
+    	paddingTop: "20px",
+    	paddingBottom: "20px"
     }
     return (
       <DocumentTitle title="Snippets">
@@ -39,7 +49,7 @@ class Snippet1 extends React.Component {
             <h1 className="center-block">SNIPPET1</h1>
           </div>
           <a href="/"><button id="btnClear" className="btn btn-default btn-md" style={homeBtn}>Home</button></a>
-          <div className="snippets-container " id={0} >{this.state.snippet1.snippetArr.map(function(line) {
+          <div className="snippets-container" style={snipCont} id={0} >{this.state.snippet1.snippetArr.map(function(line) {
               console.log(line);
               return <p  key={line.id} style={lines}>
                        {line.content}
