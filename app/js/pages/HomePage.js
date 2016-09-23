a62a20356ca73b0463fa3731af907bec9b718c5d
Jonathan Schupak <jrschupak@gmail.com>
2016-09-23 15:45:04 -0400

    change a href to link button snippet1

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -37,7 +37,8 @@ class HomePage extends React.Component {
         <Header /> 
         <div className="row" style={row}>
           <div className="col-sm-12 text-center" >
-            <a href="/snippet1"><button id="btnClear" className="btn btn-primary btn-md center-block" style={btn} value={0}>React.js constructor to create states</button></a>
+            <Link to="/display"></Link>
+            <Link to="/snippet1"><button id="btnClear" className="btn btn-primary btn-md center-block" style={btn} value={0}>React.js constructor to create states</button></Link>
             <a href="/snippet2"><button id="btnClear" className="btn btn-danger btn-md center-block" style={btn}  value={1}>Hello World </button></a>
             <a href="/snippet3"><button id="btnClear" className="btn btn-success btn-md center-block" style={btn}  value={1}>Hello World </button></a>
             <a href="/snippet4"><button id="btnClear" className="btn btn-info btn-md center-block" style={btn}  value={1}>Hello World </button></a>
