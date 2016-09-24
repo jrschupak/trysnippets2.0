57ae14080c6d16a0aeccf85e09a212761df1076c
Jonathan Schupak <jrschupak@gmail.com>
2016-09-24 12:06:23 -0400

    setup snippet5 page

---
+++ app/js/pages/Snippet5.js
@@ -0,0 +1,57 @@
+import React 			    from "react";
+import DocumentTitle		from "react-document-title";
+import {link}			    from "react-router";
+
+class Snippet5 extends React.Component {
+
+	constructor(){
+		super();
+			this.state = {
+				snippet2: { snippetArr: [{id:0, content: "<div>"}, {id: 2, content: "This is snippet 2"}, {id: 3, content: "</div>"}]}
+			}
+	}
+
+	render() {
+
+	var lines = {
+      width: "90%",
+      textAlign: "left",
+      paddingLeft: "20px"
+    }
+
+    var homeBtn = {
+    	height: "50px",
+    	width: "100px",
+    	position: "absolute",
+    	right: "10px",
+    	top: "50px"
+    }
+
+    var snipCont = {
+    	background: "black",
+    	color: "yellow",
+    	position: "relative",
+    	top: "150px",
+    	paddingTop: "20px",
+    	paddingBottom: "20px"
+    }
+		return(
+			<DocumentTitle title="snippet3">
+				<section className="snippet3-page">
+					<h1></h1>
+					<a href="/"><button id="btnClear" className="btn btn-default btn-md" style={homeBtn}>Home</button></a>
+					<div className="snippets-container" style={snipCont} id={0} >{this.state.snippet2.snippetArr.map(function(line) {
+              			console.log(line);
+              			return <p  key={line.id} style={lines}>
+                                {line.content}
+                                <br />
+                               </p> 
+              		       })}
+          </div>
+				</section>
+			</DocumentTitle>
+			)
+	}
+}
+
+export default Snippet5;
