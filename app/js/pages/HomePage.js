c60014c656c94892a862d08d2c020badcecb2b60
Jonathan Schupak <jrschupak@gmail.com>
2016-09-25 10:03:11 -0400

    testing out position of homepage buttons

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -36,7 +36,7 @@ class HomePage extends React.Component {
         
         <Header /> 
         <div className="row" style={row}>
-          <div className="col-sm-12 text-center">
+          <div className="col-sm-10 col-sm-offset-2 text-center">
             <Link to="/snippet1"><button id="btnClear" className="btn btn-primary btn-md center-block" style={btn} value={0}>React.js <br /> constructor to create states</button></Link>
             <Link to="/snippet2"><button id="btnClear" className="btn btn-danger btn-md center-block" style={btn}  value={1}>HTML <br /> Basic container with paragraph tags </button></Link>
             <Link to="/snippet3"><button id="btnClear" className="btn btn-success btn-md center-block" style={btn}  value={1}>CSS <br /> @media syntax for responsive design</button></Link>
