25269904c5b20be77fdb44a852c426949d99f5d0
Jonathan Schupak <jrschupak@gmail.com>
2016-10-04 10:52:19 -0400

    styling for smaller screen

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -6,6 +6,7 @@ import DocumentTitle from 'react-document-title';
 import Form          from "./../components/Form.js";
 import Snippets      from "./../components/Snippets.js";
 import Header        from "./../components/Header.js";
+import MediaQuery    from "react-responsive";
 
 const propTypes = {
   currentUser: React.PropTypes.object
@@ -35,7 +36,9 @@ class HomePage extends React.Component {
         <section className="home-page">
         
         <Header /> 
-        <div className="row" style={row}>
+        
+        
+        <div className="row buttons" style={row}>
           <div className="col-sm-10 col-sm-offset-1 text-center">
             <Link to="/snippet1"><button id="btnClear" className="btn btn-primary btn-md center-block" style={btn} value={0}>React.js <br /> constructor to create states</button></Link>
             <Link to="/snippet2"><button id="btnClear" className="btn btn-danger btn-md center-block" style={btn}  value={1}>HTML <br /> Basic container with paragraph tags </button></Link>
@@ -44,6 +47,7 @@ class HomePage extends React.Component {
             <Link to="/snippet5"><button id="btnClear" className="btn btn-info btn-md center-block" style={btn}  value={1}>Hello World </button></Link>
           </div>
         </div>
+        
         </section>
       </DocumentTitle>
     );
