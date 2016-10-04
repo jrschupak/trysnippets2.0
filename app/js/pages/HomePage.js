5dce8c318ee6c6ce95c9342f5d5b4f104e14e438
Jonathan Schupak <jrschupak@gmail.com>
2016-10-04 17:35:12 -0400

    change button color

--- app/js/pages/HomePage.js
+++ app/js/pages/HomePage.js
@@ -40,11 +40,11 @@ class HomePage extends React.Component {
         
         <div className="row buttons" style={row}>
           <div className="col-sm-10 col-sm-offset-1 text-center">
-            <Link to="/snippet5"><button id="btnClear" className="btn btn-info btn-md center-block" style={btn}  value={1}>Input pop-up container <br /> HTML, JS, CSS </button></Link>
+            <Link to="/snippet5"><button id="btnClear" className="btn btn-warning btn-md center-block" style={btn}  value={1}>Input pop-up container <br /> HTML, JS, CSS </button></Link>
             <Link to="/snippet1"><button id="btnClear" className="btn btn-primary btn-md center-block" style={btn} value={0}>React.js <br /> constructor to create states</button></Link>
             <Link to="/snippet2"><button id="btnClear" className="btn btn-danger btn-md center-block" style={btn}  value={1}>HTML <br /> Basic container with paragraph tags </button></Link>
             <Link to="/snippet3"><button id="btnClear" className="btn btn-success btn-md center-block" style={btn}  value={1}>CSS <br /> @media syntax for responsive design</button></Link>
-            <Link to="/snippet4"><button id="btnClear" className="btn btn-warning btn-md center-block" style={btn}  value={1}>JavaScript <br /> Math.random function </button></Link>
+            <Link to="/snippet4"><button id="btnClear" className="btn btn-info btn-md center-block" style={btn}  value={1}>JavaScript <br /> Math.random function </button></Link>
             
           </div>
         </div>
