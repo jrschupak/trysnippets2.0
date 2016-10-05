b541840d3c9838e40d1887787a4c3a14e0f3b2c8
Jonathan Schupak <jrschupak@gmail.com>
2016-10-05 11:47:08 -0400

    position of snippet5 display code

--- app/js/pages/Snippet5.js
+++ app/js/pages/Snippet5.js
@@ -10,7 +10,7 @@ class Snippet5 extends React.Component {
 				snippet: { 
           snippetHTML: [{id:0, content: "<div class='name-of-class'>"}, {id: 2, content: "<input type='text' class='one'><br><br>"}, {id: 3, content: "<input type='text' class='two'><br><br>"}, {id: 4, content: "<input type='text' class='three'><br><br>"}, {id: 5, content: "<button class='done-button'>DONE</button>"}, {id: 6, content: "</div>"}],
           snippetJS: [{id:0, content:'var doneButton = document.querySelector(".done-button");'}, {id:1, content:'var input = document.querySelectorAll(".one, .two, .three");'}, {id:3, content:'doneButton.addEventListener("click", function(){'}, {id:4, content:'for(i = 0; i < input.length; i++){'}, {id:5, content:'if(!iAmInput[i].value.length){'}, {id:6, content:'return alert("You Must fill in all the "I am" statements to continue")'}, {id:7, content:'} else {'}, {id:8, content:'if(input[4].value.length){'}, {id:9, content:'var container = document.querySelector(".name-of-class");'}, {id:10, content: 'container.style.display = "none";'}, {id:11, content:'}'}, {id:12, content:'}'}, {id:13, content:'}'}],
-          snippetCSS: [{id:0, content:'.name-of-class {'}, {id:1, content:'background: rgba(0, 0, 0, 0.8);'}, {id:2, content:'color: white;'}, {id:3, content:'width: 700px'}, {id:4, content:'font-size: 20px'}, {id:5, content:'text-align: center;'}, {id:6, content:'padding-top: 20px;'}, {id:7, content:'padding-bottom: 30px;'}, {id:8, content:'margin: 0px;'}, {id:9, content:'}'}, {id:10, content: 'input {'}, {id: 11, content:'width: 75%;'}, {id:12, content:'}'}]
+          snippetCSS: [{id:0, content:'.name-of-class {'}, {id:1, content:'background: rgba(0, 0, 0, 0.8);'}, {id:2, content:'color: black;'}, {id:3, content:'width: 700px;'}, {id:4, content:'height: 400px;'}, {id:5, content:'font-size: 15px;'}, {id:6, content:'text-align: center;'}, {id:7, content:'padding-top: 20px;'}, {id:8, content:'padding-bottom: 30px;'}, {id:9, content:'margin: 0px;'}, {id:10, content:'}'}, {id:11, content: 'input {'}, {id: 12, content:'width: 75%;'}, {id:13, content:'}'}]
         }             
       }
 	}
@@ -60,7 +60,8 @@ class Snippet5 extends React.Component {
       textAlign: "center",
       paddingTop: "-20px",
       paddingBottom: "30px",
-      margin: "0px",
+      marginLeft: "auto",
+      marginRight: "auto",
       position: "relative",
       top: "500px"
     }
